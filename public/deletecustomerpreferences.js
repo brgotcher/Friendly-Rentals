018512a87c231a8ea8d06c9f344043c9879be28e
brgotcher <brgotcher@gmail.com>
2021-11-26 01:19:24 -0600

    preemptive debugging for customer preferences delete

---
+++ public/deletecustomerpreferences.js
@@ -0,0 +1,12 @@
+// Contains the logic for the Delete script on the customer preferences page
+// Based on the videos from Week 8 (in case you need to reference them for an explanation)
+
+function deleteCustomerPreferences(customerID, carID){
+    $.ajax({
+        url: '/customerpreferences/' + customerID + ',' + carID,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
