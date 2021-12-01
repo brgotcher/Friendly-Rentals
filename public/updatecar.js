57ed395bffe12a5e84b52870d52066cb67b00c16
brgotcher <brgotcher@gmail.com>
2021-12-01 01:30:32 -0600

    add comments and improve saveCarUpdate function in updatecar.js

--- public/updatecar.js
+++ public/updatecar.js
@@ -1,6 +1,10 @@
+// change the cells in a row of the table to text inputs with the current data prefilled
 function updateCar(id) {
+	// identify the current row based on the provided id
 	var row = document.getElementById("row" + id);
 	var cel = row.firstChild.nextSibling;
+	//iterate the cells in the row, copy the content, delete and replace with a text input, replace
+	//content as value
 	for (var i = 0; i < 6; i++) {
 		cel = cel.nextElementSibling;
 		var content = cel.textContent;
@@ -10,28 +14,40 @@ function updateCar(id) {
 		newInput.id = "row" + id + "cel" + i;
 		cel.appendChild(newInput);
 	}
+	// change the "update" button to a "save" button that will call the saveCarUpdate function
 	btn = document.getElementById("updateButton"+id);
 	btn.textContent = "Save";
 	// btn.onclick = saveCarUpdate(id);
+	// saveCarUpdate function has to be wrapped in another function in order to not be run automatically
+	// whenever updateCar is run
 	btn.onclick = function(){
 		saveCarUpdate(id);
 	}
 }
 
+// send the updated information to the database when the user hits the save button
 function saveCarUpdate(id) {
-	var row = document.getElementById("row" + id);
-	var cel = row.firstChild.nextSibling.nextElementSibling;
-	var bodyID = cel.firstElementChild.value;
-	cel = cel.nextElementSibling;
-	var make = cel.firstElementChild.value;
-	cel = cel.nextElementSibling;
-	var model = cel.firstElementChild.value;
-	cel = cel.nextElementSibling;
-	var year = cel.firstElementChild.value;
-	cel = cel.nextElementSibling;
-	var mileage = cel.firstElementChild.value;
-	cel = cel.nextElementSibling;
-	var available = cel.firstElementChild.value;
+	var row, cel, bodyID, make, model, year, mileage, available;
+	var attributes = [bodyID, make, model, year, mileage, available];
+	row = document.getElementById("row" + id);
+	cel = row.firstChild.nextSibling.nextElementSibling;
+	for (var i = 0; i < attributes.length; i++) {
+		attributes[i] = cel.firstElementChild.value;
+		cel = cel.nextElementSibling;
+	}
+	// var row = document.getElementById("row" + id);
+	// var cel = row.firstChild.nextSibling.nextElementSibling;
+	// var bodyID = cel.firstElementChild.value;
+	// cel = cel.nextElementSibling;
+	// var make = cel.firstElementChild.value;
+	// cel = cel.nextElementSibling;
+	// var model = cel.firstElementChild.value;
+	// cel = cel.nextElementSibling;
+	// var year = cel.firstElementChild.value;
+	// cel = cel.nextElementSibling;
+	// var mileage = cel.firstElementChild.value;
+	// cel = cel.nextElementSibling;
+	// var available = cel.firstElementChild.value;
 
 	$.ajax({
         url: '/cars/' + id,
